4fb1d664afb0b8c1497d701efedb5a69ff3db10b
Sergey Bobrenok <bobrofon@gmail.com>
2022-11-04 00:23:46 +0300

    Add font-lock-defaults

--- soong-mode.el
+++ soong-mode.el
@@ -31,9 +31,60 @@
 
 ;;; Code:
 
+(defconst soong-font-lock-keywords-1
+  ;; function declarations, file directives, strings and comments
+  `()
+  "Value of `font-lock-keywords' in `soong-mode' on font lock level 1.")
+
+(defconst soong-font-lock-keywords-2
+  `(,@soong-font-lock-keywords-1
+    ;; keywords, type names, named constants
+    ;; Constants
+    (,(regexp-opt '("true" "false")
+                  'symbols)
+     . 'font-lock-constant-face))
+  "Value of `font-lock-keywords' in `soong-mode' on font lock level 2.")
+
+(defconst soong-font-lock-keywords-3
+  `(,@soong-font-lock-keywords-2
+    ;; builtin function names
+    ;; Builtin target type names
+    (,(regexp-opt '("cc_benchmark"
+                    "cc_benchmark_host"
+                    "cc_binary"
+                    "cc_binary_host"
+                    "cc_defaults"
+                    "cc_genrule"
+                    "cc_library"
+                    "cc_library_headers"
+                    "cc_library_host_shared"
+                    "cc_library_host_static"
+                    "cc_library_shared"
+                    "cc_library_static"
+                    "cc_object"
+                    "cc_prebuilt_binary"
+                    "cc_prebuilt_library_shared"
+                    "cc_prebuilt_library_static"
+                    "cc_test"
+                    "cc_test_host"
+                    "cc_test_library")
+                  'symbols)
+     . 'font-lock-builtin-face))
+  "Value of `font-lock-keywords' in `soong-mode' on font lock level 3.")
+
+(defconst soong-font-lock-keywords soong-font-lock-keywords-3
+  "Default value of `font-lock-keywords' in `soong-mode'.")
+
 ;;;###autoload
 (define-derived-mode soong-mode prog-mode "Soong"
-  "Major mode for editing Soong build files.")
+  "Major mode for editing Soong build files."
+  ;; Soong convention is to use 4 spaces for indentation.
+  (setq-local tab-width 4)
+  (setq-local indent-tabs-mode nil)
+  (setq-local font-lock-defaults '((soong-font-lock-keywords
+                                    soong-font-lock-keywords-1
+                                    soong-font-lock-keywords-2
+                                    soong-font-lock-keywords-3))))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.bp\\'" . soong-mode))
