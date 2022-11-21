43e6fa57b5fe780eb37a6ed5c7aa1d1cbf1b7504
Sergey Bobrenok <bobrofon@gmail.com>
2022-11-21 22:29:13 +0300

    Bump version to 0.1.2-snapshot

--- soong-mode.el
+++ soong-mode.el
@@ -6,7 +6,7 @@
 ;; Keywords: languages
 ;; URL: https://github.com/bobrofon/soong-mode
 ;; Package-Requires: ((emacs "27.1"))
-;; Version: 0.1.1
+;; Version: 0.1.2-snapshot
 
 ;; This file is not part of GNU Emacs.
 
